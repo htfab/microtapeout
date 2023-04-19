67d06bf50d2773eb97136305e3179416420f092b
htfab <mpw@htamas.net>
2023-04-20 01:46:38 +0200

    add an empty test suite

---
+++ src/test.py
@@ -0,0 +1,7 @@
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
+
+@cocotb.test()
+async def test_microtapeout(dut):
+    dut._log.info("TODO")
